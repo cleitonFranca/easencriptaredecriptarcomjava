463928d187860e655e4f498384b51e418e4f636e
Nelson Martins <nelsontecti@gmail.com>
2016-10-06 10:44:30 -0300

    Update EasencriptareDecriptar.java

--- EasencriptareDecriptar.java
+++ EasencriptareDecriptar.java
@@ -1,3 +1,9 @@
+package easencriptaredecriptar;
+
+/**
+ * @author Nelson Martins
+ */
+
 // Class para criptografia.
 //import java.security.spec.AlgorithmParameterSpec;
 //import javax.crypto.spec.IvParameterSpec;
@@ -7,34 +13,34 @@ import javax.xml.bind.DatatypeConverter;
 
 public class EasencriptareDecriptar {
 
-static String CHAVE_ENCRIPTACAO = "0123456789abcdef"; // 128 bit
+    static String CHAVE_ENCRIPTACAO = "0123456789abcdef"; // 128 bit
 
-public static void main(String[] args) throws Exception {
-String plainText = "01mobile";
-String cipherText = encryptText(plainText);
-String decryptedText = decryptText(cipherText);
+    public static void main(String[] args) throws Exception {
+        String plainText = "nelson";
+        String cipherText = encryptText(plainText);
+        String decryptedText = decryptText(cipherText);
 
-System.out.println("Original Text:" + plainText);
-System.out.println("AES Key (Hex Form):" + CHAVE_ENCRIPTACAO);
-System.out.println("Encrypted Text (Hex Form):" + cipherText);
-System.out.println("Descrypted Text:" + decryptedText);
+        System.out.println("Texto Original:" + plainText);
+        System.out.println("AES Key (Hex Form):" + CHAVE_ENCRIPTACAO);
+        System.out.println("Texto Encripitado (Hex Form):" + cipherText);
+        System.out.println("Text Descripitado:" + decryptedText);
 
-}
+    }
 
-public static String encryptText(String plainText) throws Exception {
+    public static String encryptText(String plainText) throws Exception {
 // AES defaults to AES/ECB/PKCS5Padding in Java 7
-Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-aesCipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(CHAVE_ENCRIPTACAO.getBytes("UTF-8"), "AES"));
-byte[] byteCipherText = aesCipher.doFinal(plainText.getBytes("UTF8"));
-return DatatypeConverter.printBase64Binary(byteCipherText);
+        Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        aesCipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(CHAVE_ENCRIPTACAO.getBytes("UTF-8"), "AES"));
+        byte[] byteCipherText = aesCipher.doFinal(plainText.getBytes("UTF8"));
+        return DatatypeConverter.printBase64Binary(byteCipherText);
 
-}
+    }
 
-public static String decryptText(String byteCipherText) throws Exception {
+    public static String decryptText(String byteCipherText) throws Exception {
 // AES defaults to AES/ECB/PKCS5Padding in Java 7
-Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-aesCipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(CHAVE_ENCRIPTACAO.getBytes("UTF-8"), "AES"));
-return new String(aesCipher.doFinal(DatatypeConverter.parseBase64Binary(byteCipherText)), "UTF-8");
-}
+        Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        aesCipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(CHAVE_ENCRIPTACAO.getBytes("UTF-8"), "AES"));
+        return new String(aesCipher.doFinal(DatatypeConverter.parseBase64Binary(byteCipherText)), "UTF-8");
+    }
 
 }
